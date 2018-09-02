ece05e2774ebcae6142da83adc902579d40b75db
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-09-02 20:35:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-const expect = require('expect')
-
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-
-
-describe('index', () => {
-  
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-  })
-  
-
-  it('runs', () => {
-    expect(true).toEqual(true)
-  })
-})
-
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
